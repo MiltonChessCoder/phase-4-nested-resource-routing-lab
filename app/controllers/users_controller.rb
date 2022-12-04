38f5101121fd3f7986370805e040662ab10b583d
Milton <miltonsoftwareengineer@gmail.com>
2022-12-04 16:35:07 +0300

    updated assignment

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,20 @@
 class UsersController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :user_not_found
+
+  def index 
+    users = User.all 
+    render json: users 
+  end 
 
   def show
     user = User.find_by(id: params[:id])
-    render json: user, include: :items
+    render json: user, include: :items, status: :ok 
   end
+
+  private 
+
+  def user_not_found 
+    render json: { error: "User not found "}, status: :not_found
+  end 
 
 end
